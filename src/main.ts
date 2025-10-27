2a14b6b2db8c20ad92b7f729d5e0229e06ce95d2
habberrih <abdul2013ly@gmail.com>
2025-10-27 17:41:29 +0200

    feat: enhance main application setup with versioning and improved Swagger options

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidUnknownValues: true,
+      transform: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Minara API')
@@ -13,7 +28,12 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      customSiteTitle: 'Minara Documentation',
+    },
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
