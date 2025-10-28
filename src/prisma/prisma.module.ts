57c9dc489d4a3d80ef675a3ddadc164fecc733f4
habberrih <abdul2013ly@gmail.com>
2025-10-28 17:36:15 +0200

    feat: add TenantContextService to PrismaModule for organization-specific context management

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,10 @@
 import { Global, Module } from '@nestjs/common';
+import { TenantContextService } from 'src/tenant/tenant-context.service';
 import { PrismaService } from './prisma.service';
 
 @Global()
-@Module({ providers: [PrismaService], exports: [PrismaService] })
+@Module({
+  providers: [PrismaService, TenantContextService],
+  exports: [PrismaService, TenantContextService],
+})
 export class PrismaModule {}
