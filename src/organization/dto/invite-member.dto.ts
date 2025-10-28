8d7fb788480dffe260b8bee10862127e07171b1c
habberrih <abdul2013ly@gmail.com>
2025-10-28 17:35:54 +0200

    feat: add InviteMemberDto for organization member invitations

---
+++ src/organization/dto/invite-member.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { OrgRole } from '@prisma/client';
+import { IsEnum, IsOptional, IsUUID } from 'class-validator';
+
+export class InviteMemberDto {
+  @ApiProperty({
+    description: 'User identifier that should be added to the organization',
+    example: 'ddeb9fad-5d6a-46b8-b73f-7d29e5c50703',
+  })
+  @IsUUID()
+  userId!: string;
+
+  @ApiPropertyOptional({
+    description: 'Role to assign to the invited member. Defaults to MEMBER.',
+    enum: OrgRole,
+    example: OrgRole.MEMBER,
+  })
+  @IsOptional()
+  @IsEnum(OrgRole)
+  role?: OrgRole;
+}
